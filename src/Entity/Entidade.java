8a2b18d5938bb7c4be524fbe658136d9d8a2f5e8
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2025-11-16 01:52:22 -0300

    feat: renderização de personagem com imagens de sprites

--- src/Entity/Entidade.java
+++ src/Entity/Entidade.java
@@ -1,6 +1,13 @@
 package Entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entidade {
     public int x, y;
     public int velocidade;
+
+    public BufferedImage cima1, cima2, baixo1, baixo2, esquerda1, esquerda2, direita1, direita2;
+    public String direcao;
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
--- src/Entity/Jogador.java
+++ src/Entity/Jogador.java
@@ -3,7 +3,10 @@ package Entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Jogador extends Entidade {
     private GamePanel gamePanel;
@@ -14,35 +17,100 @@ public class Jogador extends Entidade {
         this.keyHandler = keyHandler;
 
         setValoresDefault();
+        getImagemJogador();
     }
 
     public void setValoresDefault(){
         x = 100;
         y = 100;
         velocidade = 4;
+        direcao = "baixo";
+    }
+
+    public void getImagemJogador(){
+        try {
+            cima1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            cima2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            baixo1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            baixo2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            esquerda1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            esquerda2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            direita1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            direita2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void update(){
-        if(keyHandler.upPressed){
-            y -= velocidade;
-        }
-        if(keyHandler.downPressed){
-            y += velocidade;
-        }
-        if(keyHandler.leftPressed){
-            x -= velocidade;
-        }
-        if (keyHandler.rightPressed){
-            x += velocidade;
+        if(keyHandler.upPressed == true || keyHandler.downPressed == true || keyHandler.rightPressed == true || keyHandler.leftPressed == true){
+            if(keyHandler.upPressed){
+                direcao = "cima";
+                y -= velocidade;
+            }
+            if(keyHandler.downPressed){
+                direcao = "baixo";
+                y += velocidade;
+            }
+            if(keyHandler.leftPressed){
+                direcao = "esquerda";
+                x -= velocidade;
+            }
+            if (keyHandler.rightPressed){
+                direcao = "direita";
+                x += velocidade;
+            }
+
+            spriteCounter++;
+            if(spriteCounter > 12){
+                if (spriteNum ==1 ){
+                    spriteNum =2;
+                } else if (spriteNum  == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
     }
 
     public void drawJogador(Graphics2D g2){
+        BufferedImage image = null;
 
-        g2.setColor(Color.CYAN);
-
-        g2.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
-
-        g2.dispose();
+        switch (direcao){
+            case "cima" :
+                if (spriteNum == 1){
+                    image = cima1;
+                }
+                if (spriteNum == 2){
+                    image = cima2;
+                }
+                break;
+            case "baixo":
+                if (spriteNum == 1){
+                    image = baixo1;
+                }
+                if (spriteNum == 2){
+                    image = baixo2;
+                }
+                break;
+            case "esquerda":
+                if (spriteNum == 1){
+                    image = esquerda1;
+                }
+                if (spriteNum == 2){
+                    image = esquerda2;
+                }
+                break;
+            case "direita":
+                if (spriteNum == 1){
+                    image = direita1;
+                }
+                if (spriteNum == 2){
+                    image = direita2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
     }
 }
